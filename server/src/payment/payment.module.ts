23b5b29c4da27c5baef022b31cb6aeb355de7d00
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2023-02-21 15:11:46 +0100

    import usermodule and responseHandler in payment module

--- server/src/payment/payment.module.ts
+++ server/src/payment/payment.module.ts
@@ -3,20 +3,24 @@ import { PaymentService } from './payment.service';
 import { PaymentController } from './payment.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from 'src/schemas/user.schema';
-import { Invoice, InvoiceSchema } from 'src/schemas/invoice.schema';
+import { DBInvoice, DBInvoiceSchema } from 'src/schemas/invoice.schema';
 import { Account, AccountSchema } from 'src/schemas/account.schema';
 import { Widthrawal, WidthrawalSchema } from 'src/schemas/withdrawal.schema';
 import { Reversal, ReversalSchema } from 'src/schemas/reversal.schema';
+import { UserModule } from 'src/user/user.module';
+import { ResponseHandlerModule } from 'src/response-handler/response-handler.module';
 
 @Module({
   imports:[
     MongooseModule.forFeature([
       {name:User.name,schema:UserSchema},
-      {name:Invoice.name,schema:InvoiceSchema},
+      {name:DBInvoice.name,schema:DBInvoiceSchema},
       {name:Account.name,schema:AccountSchema},
       {name:Widthrawal.name,schema:WidthrawalSchema},
       {name:Reversal.name,schema:ReversalSchema}
-    ])
+    ]),
+    UserModule,
+    ResponseHandlerModule
   ],
   providers: [PaymentService],
   controllers: [PaymentController]
