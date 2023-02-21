e1d7c404199f839c71c3cdd48a4ef736803eb226
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2023-02-21 15:08:26 +0100

    create connection with lnd node in main.tss

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { LndClientModule } from './lnd-client/lnd-client.module';
+import { LndClientService } from './lnd-client/lnd-client.service';
 
 async function bootstrap() {
   const options = {
@@ -9,12 +11,12 @@ async function bootstrap() {
   }
   
   const app = await NestFactory.create(AppModule);
-
   //enabling cors
   app.enableCors(options)
   await app.listen(process.env.PORT || 3001);
 }
 try {
+  LndClientService.instance.createConnection()
   bootstrap();
 } catch (error:unknown) {
   Logger.log(error)
