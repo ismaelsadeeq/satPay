9ce5f93831679bd67a437a09b22e77c66e2c1c0b
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2023-02-22 00:40:22 +0100

    remove reversal schema import from payment module

--- server/src/payment/payment.module.ts
+++ server/src/payment/payment.module.ts
@@ -6,7 +6,6 @@ import { User, UserSchema } from 'src/schemas/user.schema';
 import { DBInvoice, DBInvoiceSchema } from 'src/schemas/invoice.schema';
 import { Account, AccountSchema } from 'src/schemas/account.schema';
 import { Widthrawal, WidthrawalSchema } from 'src/schemas/withdrawal.schema';
-import { Reversal, ReversalSchema } from 'src/schemas/reversal.schema';
 import { UserModule } from 'src/user/user.module';
 import { ResponseHandlerModule } from 'src/response-handler/response-handler.module';
 
@@ -17,7 +16,6 @@ import { ResponseHandlerModule } from 'src/response-handler/response-handler.mod
       {name:DBInvoice.name,schema:DBInvoiceSchema},
       {name:Account.name,schema:AccountSchema},
       {name:Widthrawal.name,schema:WidthrawalSchema},
-      {name:Reversal.name,schema:ReversalSchema}
     ]),
     UserModule,
     ResponseHandlerModule
