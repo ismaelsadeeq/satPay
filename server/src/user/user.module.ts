270bc0cdeb7c134bb6aced792a5aaa266f432d26
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2023-02-22 00:41:19 +0100

    add account schema in user module imports

--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -5,11 +5,13 @@ import { UserService } from './user.service';
 import { User, UserSchema } from '../schemas/user.schema';
 import { UserController } from './user.controller';
 import { ResponseHandlerModule } from 'src/response-handler/response-handler.module';
+import { Account, AccountSchema } from 'src/schemas/account.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: User.name, schema: UserSchema },
+      { name:Account.name, schema: AccountSchema}
     ]),
     ResponseHandlerModule,
   ],
