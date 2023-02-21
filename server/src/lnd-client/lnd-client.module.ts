017620e28bbc4666eadcd0c835260c296f91231a
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2023-02-21 15:09:26 +0100

    remove unneccessary export of LndCLientService

--- server/src/lnd-client/lnd-client.module.ts
+++ server/src/lnd-client/lnd-client.module.ts
@@ -2,14 +2,12 @@ import { Module } from '@nestjs/common';
 import { LndClientService } from './lnd-client.service';
 import { ConfigModule } from '@nestjs/config';
 
-
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal:true
     }),
   ],
-  providers:[LndClientService],
-  exports: [LndClientService],
+  providers:[LndClientService]
 })
 export class LndClientModule {}
