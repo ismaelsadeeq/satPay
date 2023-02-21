5b1dfaa0682f5a8f3c033de32737b755a27d5b70
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2023-02-21 15:11:24 +0100

    create a generate invoice controller

--- server/src/payment/payment.controller.ts
+++ server/src/payment/payment.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post, Request } from '@nestjs/common';
+import { PaymentService } from './payment.service';
+import { createInvoice } from 'src/request/payment/invoice';
 
-@Controller('payment')
-export class PaymentController {}
+@Controller('api/v1/payment')
+export class PaymentController {
+
+  constructor(
+    private readonly paymentService:PaymentService
+  ){}
+
+  @Post('generate-invoice')
+  @HttpCode(HttpStatus.CREATED)
+  generateInvoice(@Request() req,@Body() generateInvoiceRequest:createInvoice ){
+    return this.paymentService.generateInvoice(generateInvoiceRequest,req.user.id);
+  }
+}
