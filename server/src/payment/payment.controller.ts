d1d4b374829eb94b7fb6b2b179196cbf7f229ee2
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2023-02-22 00:37:39 +0100

    create endpoints in payment controller and cron job
    balance, get payments, widthraw, get withrawals and update balance job

--- server/src/payment/payment.controller.ts
+++ server/src/payment/payment.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, HttpCode, HttpStatus, Post, Request } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Logger, Post, Request } from '@nestjs/common';
 import { PaymentService } from './payment.service';
-import { createInvoice } from 'src/request/payment/invoice';
+import { createInvoice, widthrawData } from 'src/request/payment/invoice';
+import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Controller('api/v1/payment')
 export class PaymentController {
@@ -11,7 +12,35 @@ export class PaymentController {
 
   @Post('generate-invoice')
   @HttpCode(HttpStatus.CREATED)
-  generateInvoice(@Request() req,@Body() generateInvoiceRequest:createInvoice ){
+  generateInvoice(@Request() req,@Body() generateInvoiceRequest:createInvoice ):Promise<any>{
     return this.paymentService.generateInvoice(generateInvoiceRequest,req.user.id);
   }
+  @Get('get-balance')
+  @HttpCode(HttpStatus.CREATED)
+  getBalance(@Request() req ){
+    return this.paymentService.getBalance(req.user.id);
+  }
+
+  @Get('get-user-payments')
+  @HttpCode(HttpStatus.CREATED)
+  getUserPayments(@Request() req ):Promise<any>{
+    return this.paymentService.getUserPayments(req.user.id);
+  }
+
+  @Post('widthraw')
+  @HttpCode(HttpStatus.CREATED)
+  widthrawMSat(@Body() widthrawData:widthrawData,@Request() req):Promise<any>{
+    return this.paymentService.widthrawData(widthrawData,req.user.id)
+  }
+
+  @Get('get-widthrawals')
+  @HttpCode(HttpStatus.CREATED)
+  getWidthrawal(@Request() req):Promise<any>{
+    return this.paymentService.getWidthrawals(req.user.id)
+  }
+  @Cron(CronExpression.EVERY_10_SECONDS)
+  runEvery10Seconds(){
+    return this.paymentService.checkInvoices();
+  }
+
 }
