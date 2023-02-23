50ee3e598f979ad917e3692c4af6a3dc85f1ac15
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2023-02-23 21:27:09 +0100

    modify the client theme colors

--- client/src/styles/Theme.tsx
+++ client/src/styles/Theme.tsx
@@ -19,6 +19,7 @@ function responsiveFontSizes({ sm, md, lg }:any) {
     },
   };
 }
+
 // SETUP COLORS
 const GREY = {
   0: '#FFFFFF',
@@ -31,21 +32,29 @@ const GREY = {
   700: '#454F5B',
   800: '#212B36',
   900: '#161C24',
+  500_8: alpha('#919EAB', 0.08),
+  500_12: alpha('#919EAB', 0.12),
+  500_16: alpha('#919EAB', 0.16),
+  500_24: alpha('#919EAB', 0.24),
+  500_32: alpha('#919EAB', 0.32),
+  500_48: alpha('#919EAB', 0.48),
+  500_56: alpha('#919EAB', 0.56),
+  500_80: alpha('#919EAB', 0.8),
 };
 
 const PRIMARY = {
-  lighter: '#feef7b',
-  light: '#fee569',
-  main: '#fdd142',
-  dark: '#bbae28',
-  darker: '#aable',
+  lighter: '#D1E9FC',
+  light: '#76B0F1',
+  main: '#2065D1',
+  dark: '#103996',
+  darker: '#061B64',
   contrastText: '#fff',
 };
 
 const SECONDARY = {
-  lighter: '#aable',
+  lighter: '#D6E4FF',
   light: '#84A9FF',
-  main: '#aable',
+  main: '#3366FF',
   dark: '#1939B7',
   darker: '#091A7A',
   contrastText: '#fff',
@@ -86,37 +95,33 @@ const ERROR = {
   darker: '#7A0C2E',
   contrastText: '#fff',
 };
+
 const palette = {
   common: { black: '#000', white: '#fff' },
-  primary: PRIMARY,
-  secondary: SECONDARY,
-  info: INFO,
-  success: SUCCESS,
-  warning: WARNING,
-  error: ERROR,
+  primary: { ...PRIMARY },
+  secondary: { ...SECONDARY },
+  info: { ...INFO },
+  success: { ...SUCCESS },
+  warning: { ...WARNING },
+  error: { ...ERROR },
   grey: GREY,
-  divider: alpha(GREY[500], 0.24),
-  text: {
-    primary: GREY[800],
-    secondary: GREY[600],
-    disabled: GREY[500],
-  },
-  background: {
-    paper: '#20262E',
-    default: "#20262E",
-    neutral: GREY[200],
-  },
+  // gradients: GRADIENTS,
+  // chart: CHART_COLORS,
+  divider: GREY[500_24],
+  text: { primary: GREY[800], secondary: GREY[600], disabled: GREY[500] },
+  background: { paper: '#fff', default: GREY[100], neutral: GREY[200] },
   action: {
     active: GREY[600],
-    hover: alpha(GREY[500], 0.08),
-    selected: alpha(GREY[500], 0.16),
-    disabled: alpha(GREY[500], 0.8),
-    disabledBackground: alpha(GREY[500], 0.24),
-    focus: alpha(GREY[500], 0.24),
+    hover: GREY[500_8],
+    selected: GREY[500_16],
+    disabled: GREY[500_80],
+    disabledBackground: GREY[500_24],
+    focus: GREY[500_24],
     hoverOpacity: 0.08,
     disabledOpacity: 0.48,
   },
 };
+
 const color = palette.grey[500];
 const transparent1 = alpha(color, 0.2);
 const transparent2 = alpha(color, 0.14);
