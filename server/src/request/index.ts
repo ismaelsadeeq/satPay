8e89a0893aa810e418a1f29938e3d869e8a71e7d
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2023-03-03 00:20:45 +0100

    export update user request

--- server/src/request/index.ts
+++ server/src/request/index.ts
@@ -2,3 +2,4 @@ export { User } from './user.model';
 
 export { LoginRequest } from './auth/login-request.model';
 export { SignupRequest } from './auth/signup-request.model';
+export {updateUserRequest} from './auth/signup-request.model'
